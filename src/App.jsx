74e7affbc9464038f0738665559694f720ea6cf6
Maria <limamariaeduarda915@gmail.com>
2025-08-28 11:16:45 -0300

    finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -49,32 +49,26 @@ const translations = {
   en: {
     overviewTitle: "OVERVIEW",
     overviewText1:
-      "Despite the growth of clean energy sources, most energy still comes from polluting sources, while global demand continues to rise.",
-    overviewText2: "In this scenario, ",
-    overviewText3: "technologies that harness human movement",
-    overviewText4: ", such as Power Walk, emerge as a sustainable and promising alternative.",
-    feature1: "Applicable in high-traffic areas",
-    feature2: "Use of urban space",
-    feature3: "Clean Energy Generation",
+      "Power Solutions (PS), the area responsible for the production of fuel supply and transmission systems for vehicles within Bosch, needs to ensure efficiency in testing the fuel pump flange terminals.",
+    card1Text: "Cycle time reduction from <strong>4 hours to 38 seconds</strong>",
+    card2Text: "Reduced monthly waste from <strong>40 hours</strong> to just <strong>6 minutes</strong>",
+    card3Text: "Reduction <strong>from 9 operators to 3</strong>",
     solutionTitle: "SOLUTION",
     solutionText:
-      "Power Walk transforms previously underutilized urban spaces into clean energy sources. The technology harnesses everyday movements, such as walking, to generate electricity, reducing dependence on polluting sources and contributing to environmental preservation.",
+      "As an effective solution, after an incident involving the arrival of a defective product to the customer, we developed a machine with an automated system to perform product tests, the Smart Tester. The Smart Tester also features monitoring software that allows data visualization for future analysis. Along with the machine, we created a training manual for employees and to facilitate the implementation of the new methodology.",
     developmentTitle: "DEVELOPMENT",
     teamTitle: "TEAM",
   },
   pt: {
     overviewTitle: "VISÃO GERAL",
     overviewText1:
-      "Apesar do crescimento das fontes de energia limpa, a maior parte da energia ainda provém de fontes poluentes, enquanto a demanda global continua a aumentar.",
-    overviewText2: "Nesse cenário, ",
-    overviewText3: "tecnologias que aproveitam o movimento humano",
-    overviewText4: ", como a Power Walk, surgem como uma alternativa sustentável e promissora.",
-    feature1: "Aplicável em áreas de grande circulação",
-    feature2: "Aproveitamento do espaço urbano",
-    feature3: "Geração de Energia Limpa",
+      "A Power Solutions (PS), área responsável pela produção de sistemas de alimentação e transmissão de combustível para veículos dentro da Bosch, precisa garantir a eficiência nos testes dos terminais da flange da bomba de combustível.",
+    card1Text: "Redução do tempo de ciclo de <strong>4 horas para 38 segundos</strong>",
+    card2Text: "Redução do desperdício mensal de <strong>40 horas</strong> para apenas <strong>6 minutos</strong>",
+    card3Text: "Redução de <strong>9 operadores para 3</strong>",
     solutionTitle: "SOLUÇÃO",
     solutionText:
-      "A Power Walk transforma espaços urbanos antes subutilizados em fontes de energia limpa. A tecnologia aproveita movimentos cotidianos, como o caminhar, para gerar eletricidade, reduzindo a dependência de fontes poluentes e contribuindo para a preservação ambiental.",
+      "Como solução eficaz, após um incidente envolvendo a chegada de um produto defeituoso ao cliente, desenvolvemos uma máquina com sistema automatizado para realizar testes nos produtos, o Smart Tester. O Smart Tester também possui um software de monitoramento que permite a visualização de dados para análises futuras. Juntamente com a máquina, criamos um manual de treinamento para os colaboradores e para facilitar a implementação da nova metodologia.",
     developmentTitle: "DESENVOLVIMENTO",
     teamTitle: "EQUIPE",
   },
@@ -86,91 +80,76 @@ export default function App() {
   const t = translations[language];
 
   return (
-    <div className="bg-[#00242D] text-white font-poppins min-h-screen overflow-x-hidden">
+    <div className="bg-[linear-gradient(to_bottom,_white_20%,_#66be83,_#50a4d5)] text-gray-800 font-poppins min-h-screen overflow-x-hidden relative">
+      <img
+        src="/mockup.png"
+        alt="Mockup"
+        className="absolute top-0 left-0 w-full h-[450px] object-cover z-0 opacity-40"
+      />
       {/* Header */}
-      <header className="relative w-full py-4 fixed top-0 left-0 bg-[#00242D] z-50">
-        <img
-          src="/vector2.png"
-          alt="Fundo"
-          className="absolute top-0 left-0 w-60 md:w-[400px] lg:w-[500px] max-h-[90vh] object-contain z-0 opacity-50"
-        />
-        <img
-          src="/efeito.png"
-          alt="Brilho"
-          className="absolute top-0 left-0 w-[500px] h-auto object-contain opacity-40 z-0"
-        />
-        <img
-          src="/mockup.png"
-          alt="Mockup"
-          className="absolute top-16 left-0 w-48 md:top-24 md:left-4 md:w-64 object-contain z-0"
-        />
-        <img
-          src="/logo.png"
-          alt="Logo"
-          className="absolute top-10 left-1/2 -translate-x-1/2 w-48 max-w-[200px] md:top-16 md:max-w-[300px] object-contain z-0"
-        />
+      <header className="relative w-full py-4 fixed top-0 left-0 z-50">
+        <h1
+          className="absolute top-4 left-4 text-xl md:text-2xl font-semibold z-10 whitespace-nowrap"
+        >
+          <span style={{ color: '#007A42' }}>SMART</span>{' '}
+          <span style={{ color: '#0067A5' }}>CENTER</span>
+        </h1>
+
         <div className="max-w-7xl mx-auto flex items-center justify-end px-4 sm:px-6 relative z-10">
           <button
             onClick={toggleLanguage}
-            className="bg-[#2AE8D9]/20 border border-[#2AE8D9] text-white font-semibold p-3 rounded-lg hover:bg-[#2AE8D9]/40 transition-colors duration-300 flex items-center justify-center"
+            className="text-[#0067A5] p-3 rounded-lg transition-colors duration-300 flex items-center justify-center"
           >
             <TranslateIcon className="h-5 w-5" />
           </button>
         </div>
       </header>
 
-{/* Hero */}
-<div className="relative flex flex-col md:flex-row items-start max-w-7xl mx-auto px-4 sm:px-6 pt-[400px] md:pt-40 gap-12">
-  {/* Texto */}
-  <div className="w-full md:w-1/2 text-center relative z-10">
-    <h2 className="text-3xl sm:text-4xl md:text-5xl font-bold mb-6 text-white">
-      {t.overviewTitle}
-    </h2>
-    <p className="text-base sm:text-lg md:text-xl mb-4 leading-relaxed">
-      {t.overviewText1}
-    </p>
-    <p className="text-base sm:text-lg md:text-xl leading-relaxed">
-      {t.overviewText2}
-      <strong className="font-semibold text-teal-300">{t.overviewText3}</strong>
-      {t.overviewText4}
-    </p>
-  </div>
-</div>
-
-
+      {/* Hero */}
+      <div className="relative flex flex-col md:flex-row items-center justify-center max-w-7xl mx-auto px-4 sm:px-6 pt-[470px] gap-12">
+        {/* Texto */}
+        <div className="w-full md:w-3/4 text-center relative z-10">
+          <h2 className="text-3xl sm:text-4xl md:text-5xl font-bold mb-6 text-black">
+            {t.overviewTitle}
+          </h2>
+          <p className="text-base sm:text-lg md:text-xl mb-4 leading-relaxed text-black">
+            {t.overviewText1}
+          </p>
+        </div>
+      </div>
 
       {/* Destaques */}
-      <div className="py-8 bg-[#00242D]">
-      <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex flex-col sm:flex-row justify-center items-center sm:items-stretch gap-4 md:gap-6 text-center">
-          <div className="bg-gray-800/20 border border-gray-600 rounded-2xl p-4 md:p-8 w-full sm:w-1/3 flex flex-col items-center justify-center">
-            <LocationIcon className="text-white mb-2 md:mb-4 h-8 w-8 md:h-12 md:w-12" />
-            <p className="text-sm sm:text-base md:text-lg text-white/80 px-2">{t.feature1}</p> {/* Adicionei px-2 */}
-          </div>
-          <div className="bg-[#2AE8D9]/20 border border-[#2AE8D9] rounded-2xl p-4 md:p-8 w-full sm:w-1/3 flex flex-col justify-center">
-            <p className="text-3xl sm:text-4xl md:text-6xl font-bold text-[#2AE8D9] mb-2">70%</p>
-            <p className="text-sm sm:text-base md:text-lg text-white/80 px-2">{t.feature2}</p> {/* Adicionei px-2 */}
-          </div>
-          <div className="bg-[#2AE8D9]/20 border border-[#2AE8D9] rounded-2xl p-4 md:p-8 w-full sm:w-1/3 flex flex-col justify-center">
-            <p className="text-3xl sm:text-4xl md:text-6xl font-bold text-[#2AE8D9] mb-2">100%</p>
-            <p className="text-sm sm:text-base md:text-lg text-white/80 px-2">{t.feature3}</p> {/* Adicionei px-2 */}
+      <div className="py-8">
+        <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="flex flex-col sm:flex-row justify-center items-stretch gap-4 md:gap-6 text-center">
+            {/* Card 1 */}
+            <div className="bg-[#E4EDFB] p-6 md:p-8 w-full sm:w-1/3 flex flex-col items-center justify-center text-lg md:text-xl">
+              <p dangerouslySetInnerHTML={{ __html: t.card1Text }} />
+            </div>
+            {/* Card 2 */}
+            <div className="bg-[#E4EDFB] p-6 md:p-8 w-full sm:w-1/3 flex flex-col items-center justify-center text-lg md:text-xl">
+              <p dangerouslySetInnerHTML={{ __html: t.card2Text }} />
+            </div>
+            {/* Card 3 */}
+            <div className="bg-[#E4EDFB] p-6 md:p-8 w-full sm:w-1/3 flex flex-col items-center justify-center text-lg md:text-xl">
+              <p dangerouslySetInnerHTML={{ __html: t.card3Text }} />
+              <img src="/bonequinhos.png" alt="Ícone de operadores" className="mt-4 h-12" />
+            </div>
           </div>
         </div>
       </div>
-    </div>
-
 
       {/* Solução */}
-      <div className="py-16 bg-[#00242D] px-4 sm:px-6">
+      <div className="py-16 px-4 sm:px-6">
         <div className="max-w-4xl mx-auto text-center">
-          <h2 className="text-3xl sm:text-4xl md:text-5xl font-bold mb-6 text-white">{t.solutionTitle}</h2>
-          <p className="text-base sm:text-lg md:text-xl mb-4 leading-relaxed text-white/80">{t.solutionText}</p>
+          <h2 className="text-3xl sm:text-4xl md:text-5xl font-bold mb-6 text-black">{t.solutionTitle}</h2>
+          <p className="text-base sm:text-lg md:text-xl mb-4 leading-relaxed text-gray-800">{t.solutionText}</p>
         </div>
       </div>
 
       {/* Ferramentas */}
-      <div className="flex flex-col items-center py-16 bg-[#00242D] px-4">
-        <h2 className="text-3xl sm:text-4xl md:text-5xl font-bold mb-6 text-white text-center">{t.developmentTitle}</h2>
+      <div className="flex flex-col items-center py-16 px-4">
+        <h2 className="text-3xl sm:text-4xl md:text-5xl font-bold mb-6 text-black text-center">{t.developmentTitle}</h2>
         <img
           src="/tools.png"
           alt="Ferramentas utilizadas"
@@ -179,20 +158,15 @@ export default function App() {
       </div>
 
       {/* Rodapé */}
-      <footer className="relative bg-[#00242D] pt-10 overflow-hidden px-4 sm:px-6">
+      <footer className="relative pt-10 overflow-hidden px-4 sm:px-6">
         <img
-          src="/efeito.png"
-          alt="Brilho"
-          className="absolute top-0 left-0 w-[500px] h-auto object-contain opacity-40 z-0"
-        />
-        <img
-          src="/vector3.png"
+          src="/decoração.png"
           alt="Decorativo"
           className="absolute top-0 right-0 h-full w-auto object-cover opacity-30 z-0"
         />
 
         <div className="relative z-10 max-w-7xl mx-auto">
-          <div className="border-t border-white/20"></div>
+          <div className="border-t border-black/20"></div>
 
           <div className="flex flex-col md:flex-row items-center justify-between gap-6 py-8 text-center">
             <div className="flex items-center gap-6 justify-center">
@@ -200,11 +174,11 @@ export default function App() {
               <img src="/bosch.png" alt="Bosch" className="h-12" />
             </div>
 
-            <div className="w-px h-12 bg-white/30 hidden md:block"></div>
+            <div className="w-px h-12 bg-black/30 hidden md:block"></div>
 
             <div>
-              <p className="font-bold tracking-wider text-white mb-1">{t.teamTitle}</p>
-              <p className="text-white/80">Caio Santos, Cláudio Araujo, Leandro Castro, Miguel Socha</p>
+              <p className="font-bold tracking-wider text-black mb-1">{t.teamTitle}</p>
+              <p className="text-black">Alisson Ribeiro, Matheus Belline, Pedro Lucas e Pedro Pereira</p>
             </div>
           </div>
         </div>
